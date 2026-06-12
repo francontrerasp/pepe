2d6c7d9677f96bdecd7b3f0cf86bb2257433f40d
francontrerasp <fran.contrerasp@duocuc.cl>
2026-06-12 10:17:38 -0400

    cree lista de alumnos

--- ejer1.py
+++ ejer1.py
@@ -8,3 +8,24 @@ un curso en particular.
 Como prototipo, usted desarrolla un algoritmo que permite
 almacenar un número variable de alumnos a un curso, pero con
 un máximo de 30 por curso """
+
+while True:
+    try:
+        cant_alumnos = int(input("Ingrese la cantidad de alumnos a registrar: "))
+        if cant_alumnos > 0 and cant_alumnos <= 30:
+            break
+        raise ValueError
+    except:
+        print("La cantidada no puede ser superior a 30 y debe ser un numero entero positivo")
+
+lista_alumnos = []
+
+for i in range(cant_alumnos):
+    print("------Datos del alumno nro------ ", i+1)
+    alumno = {}
+    alumno["nombre"] = input("ingrese nombre del alumno: ")
+    alumno["direccion"] = input("ingrese direccion de alumno: ")
+    alumno["telefono"] = input("ingrese telefono de alumno: ")
+    lista_alumnos.append(alumno)
+
+print(lista_alumnos)
